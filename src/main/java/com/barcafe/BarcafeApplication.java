f59ccc5d30cccd4c4b286f136588767886320a28
Javier <jaasmu.9@gmail.com>
2026-05-26 03:47:00 -0500

    create Basic main class

--- src/main/java/com/barcafe/BarcafeApplication.java
+++ src/main/java/com/barcafe/BarcafeApplication.java
@@ -1,4 +1,11 @@
 package com.barcafe;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.barcafe")
 public class BarcafeApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BarcafeApplication.class, args);
+    }
 }
