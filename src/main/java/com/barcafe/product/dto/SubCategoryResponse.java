ae64691908de1066009d978290dcfd72bd4fa2eb
Javier <jaasmu.9@gmail.com>
2026-05-26 17:37:40 -0500

    Fix typo

--- src/main/java/com/barcafe/product/dto/SubCategoryResponse.java
+++ src/main/java/com/barcafe/product/dto/SubCategoryResponse.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 @Getter
 @Builder
-public class SunCategoryResponse {
+public class SubCategoryResponse {
     private UUID id;
     private String name;
     private String description;
